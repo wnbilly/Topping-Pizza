8e01d8132d2c252085105065bb36c596ae0e3fdd
Seb-sti1 <65665540+seb-sti1@users.noreply.github.com>
2023-01-21 18:11:42 +0100

    server file for efficient net

---
+++ server_efficientNet.py
@@ -0,0 +1,217 @@
+DATA_DIR =  './pizzaGANsyntheticdata'
+
+import pandas as pd
+import numpy as np
+import os
+import time
+import matplotlib.pyplot as plt
+from PIL import Image
+
+from tqdm import tqdm
+
+import seaborn as sns
+sns.set_style('darkgrid')
+
+#import shutil
+
+import sys
+if not sys.warnoptions:
+    import warnings
+    warnings.simplefilter("ignore")
+
+# scikitlearn
+from sklearn.metrics import multilabel_confusion_matrix, classification_report, f1_score, accuracy_score
+from sklearn.model_selection import train_test_split
+
+# Pytorch
+import torch
+from torch.utils.data import Dataset, DataLoader
+import torchvision.transforms as transforms
+import torchvision
+import torchvision.transforms as transforms
+from torchvision import models
+import torch.optim as optim
+# from torch.optim import lr_scheduler
+import torch.nn.functional as F
+import torch.nn as nn
+
+import sys, select
+
+#!pip install torchmetrics
+#from torchmetrics.classification import MultilabelF1Score, MultilabelAccuracy
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+if (torch.cuda.is_available()):
+  !nvidia-smi
+  
+  
+class myDataset(Dataset):
+    """Pizza dataset"""
+    
+    def __init__(self, x_idx, y, img_path='/content/gdrive/MyDrive/Colab Notebooks/syntheticDataset/train/images', img_data = None, transform=None):
+      """
+      Args:
+      """
+      self.x_idx = x_idx
+      self.y = y
+      self.img_path = img_path
+      self.transform = transform
+      self.img_data = img_data     
+    
+    def __getitem__(self, idx):
+      if isinstance(self.img_data,np.ndarray):
+        x = Image.fromarray(self.img_data[idx,:,:,:])
+      else:
+        img_name = "{:04d}.png".format(idx+1)
+        x = Image.open(os.path.join(self.img_path, img_name))
+      y = self.y[idx,:]
+      if self.transform:
+          x = self.transform(x)
+      y = np.int64(y)
+      return x, y
+            
+    def __len__(self):
+        return int(len(self.x_idx))
+
+
+# ===================================== CREATE DATASETs
+
+
+batch_size = 25
+
+input_size = 224
+
+normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                 std=[0.229, 0.224, 0.225])
+
+train_transform = transforms.Compose([transforms.RandomHorizontalFlip(),
+                                         transforms.RandomVerticalFlip(),
+                                         transforms.ColorJitter(brightness=32. / 255., saturation=0.5),
+                                         transforms.Resize(input_size),
+                                         transforms.ToTensor(),
+                                     normalize])
+
+val_transform = transforms.Compose([transforms.Resize(input_size),
+                                    transforms.ToTensor(),
+                                   normalize])
+        
+kwargs = {'num_workers': 2, 'pin_memory': True} if torch.cuda.is_available() else {}
+
+train_data_path=img_path = os.path.join(DATA_DIR, 'syntheticDataset/train/images')
+
+img_data=np.array(img_all)
+img_data_train = img_data[x_train,:,:,:]
+train_set_raw = myDataset(x_train, y_train, img_path=train_data_path, img_data=img_data_train, transform = train_transform)
+train_dataloader = DataLoader(train_set_raw, batch_size=batch_size, shuffle=True, **kwargs)
+
+img_data_val = img_data[x_val,:,:,:]
+val_set_raw = myDataset(x_val, y_val, img_path=train_data_path, img_data=img_data_val, transform = val_transform)
+val_dataloader = DataLoader(val_set_raw, batch_size=batch_size, shuffle=False, **kwargs)
+
+test_data_path=img_path = os.path.join(DATA_DIR, 'syntheticDataset/test/images')
+
+test_set_raw = myDataset(x_test, y_test, img_path=test_data_path, transform = val_transform)
+test_dataloader = DataLoader(test_set_raw, batch_size=batch_size, shuffle=False, **kwargs)
+
+
+nlabel = y_all.shape[1]
+
+
+# ========================= NETWORK
+
+
+from torchvision.models import efficientnet_b0, EfficientNet_B0_Weights
+
+network = torchvision.models.efficientnet_b0(weights=EfficientNet_B0_Weights.DEFAULT)
+num_ftrs = network.classifier[1].in_features
+network.classifier[1]= nn.Linear(num_ftrs, nlabel)
+
+# Name of the network
+tag = "efficientNet"
+
+# ======================================= TRAIN LOOP
+
+#from sklearn.metrics import confusion_matrix
+from IPython.display import clear_output
+
+import copy
+import random
+
+testloader = test_dataloader
+
+def train_model_multilabel(model, nlabel, trainloader, valloader, criterion, optimizer, scheduler, num_epochs=5):
+  # list for saving accuracies
+  train_perf = []
+  test_perf = []
+  train_losses = []
+
+
+  for epoch in range(num_epochs): # on va iterer sur toutes les données num_epochs fois
+      print("Epoch {}".format(epoch))
+      model.train()
+
+      for inputs, targets in trainloader: # on itère sur les données par batch de batch_size (= 25)
+          inputs, targets = inputs.cuda()[0,:,:,:],targets.cuda()[0,:] # 25x3x224x224 et 25x10
+
+          predictions = model(inputs)    ## on les fait rentrer dans le réseau
+          targets = targets.to(torch.float) # FloatTensor needed
+
+          loss = criterion(predictions,targets)    ## on compare la sortie courante à la sortie voulue
+          optimizer.zero_grad() ## supprime les gradients courants
+          loss.backward() ## le gradient -- la magie par rapport à comment c'était long en cours :-)
+          optimizer.step() ## on actualise les poids pour que la sortie courante soit plus proche que la sortie voulue
+          #train_losses.append(loss)
+          if random.randint(0,90)==0:
+              print("\tloss=",loss) ## on affiche pour valider que ça diverge pas
+      # Learning step
+
+  return model, train_perf, test_perf, train_losses
+
+# ====================================================== TRAIN ROUTINE
+
+ # Define data loaders
+trainloader=train_dataloader
+valloader=val_dataloader
+
+# Read the last learned network (if stored)
+if (os.path.exists(os.path.join("Networks", 'network_{:s}.pth'.format(tag)))):
+    print('Resume from last learning step')
+    network = torch.load(os.path.join("Networks", 'network_{:s}.pth'.format(tag)))
+
+# Transfer network to GPU
+network.to(device)
+
+# Define learning components (to be used in the learning function)
+
+optimizer = optim.Adam(network.parameters(), lr=0.00001)
+criterion = nn.CrossEntropyLoss()
+scheduler = None
+
+i = 0
+
+while True:
+	max_epoch = 5 # initial 20
+	print(f"Total Epoch {i*max_epoch}")
+	
+	# Learning
+	learned_model, train_error, test_error, train_losses = train_model_multilabel(network, nlabel, trainloader, valloader, criterion, optimizer, scheduler, num_epochs=max_epoch)
+	torch.save(learned_model, os.path.join("Networks", 'network_{:s}_{:d}.pth'.format(tag, i)))
+
+	network = learned_model
+	print("You have 1 second to answer!")
+
+	should_stop, _, _ = select.select([sys.stdin], [], [], 1)
+
+	if should_stop:
+	  print("You said something. Stoping!")
+	  break
+	else:
+	  print("You said nothing! Continuing!")
+	
+	i += 1
+
+	
+	
+	
+
